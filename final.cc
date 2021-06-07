fa75d3c5495b72a675f4c454d5de03a92a47106d
cuishaochen <cui.sc@cidi.ai>
2021-06-07 19:04:47 +0800

    final: update

--- final.cc
+++ final.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 
+// 1. final修饰函数
 class Base {
  public:
   virtual void print() = 0;
@@ -14,6 +15,16 @@ class B : public A {
  public:
 //   void print() { std::cout << "B -- final" << std::endl; }
 };
+
+// 2. final修饰类
+class Son final: public Base{
+  public:
+} ;
+
+// class GrandSon: public Son{
+//   public:
+
+// };
 
 int main(int argc, char **argv) { 
     
